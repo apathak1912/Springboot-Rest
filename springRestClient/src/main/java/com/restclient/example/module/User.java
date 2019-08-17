ef6dfcb8dd54a17d7c6fc3873263ce5cbae0dac2
Anuj <apathak1912@gmail.com>
2019-08-18 04:53:41 +0530

    first commit for clientcode

---
+++ springRestClient/src/main/java/com/restclient/example/module/User.java
@@ -0,0 +1,46 @@
+package com.restclient.example.module;
+
+
+public class User {
+
+	public User(long id, String country, String name) {
+		super();
+		this.id = id;
+		this.country = country;
+		this.name = name;
+	}
+
+	private long id;
+
+	
+	private String country;
+	
+	
+	private String name;
+	
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
